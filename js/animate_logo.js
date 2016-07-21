4e242a08a94742333c22104f1f13b8de7c236e76
Kyle Stengline <kyle.stengline@gmail.com>
2016-07-21 15:01:03 -0400

    adds fadein fadeout with jquery on pedul logo

--- js/animate_logo.js
+++ js/animate_logo.js
@@ -1,9 +1,20 @@
-(document).ready(function() {
+  console.log('loaded');
 
+  var edu = $("#edu");
+  var pandl =  $("#pandl");
+  edu.hide();
+  pandl.hide();
   
+$(document).ready(function() {
 
-
-
-
-
+  $("#edu").fadeIn(2000);
+    setTimeout(function(){
+      $("#pandl").fadeIn(1700);
+    }, 1200);
+    
+    setTimeout(function(){
+      edu.fadeOut(2500)
+      pandl.fadeOut(1000)
+    },3500);
+    
 });
