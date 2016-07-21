c3ee572a0125134801d568cec0da9835b59cad89
Kyle Stengline <kyle.stengline@gmail.com>
2016-07-21 17:12:46 -0400

    adds pedul logo fadein and fadeout to index

--- js/animate_logo.js
+++ js/animate_logo.js
@@ -1,20 +1,35 @@
-  console.log('loaded');
+var edu = $("#edu");
+var pandl =  $("#pandl");
+var pedul_logo = $("#pedul_logo_container");
+var intro_text = $(".intro-text");
+var pedul_head = $("a.page-scroll.log");
+pedul_head.hide();
+intro_text.hide();
+edu.hide();
+pandl.hide();
 
-  var edu = $("#edu");
-  var pandl =  $("#pandl");
-  edu.hide();
-  pandl.hide();
-  
 $(document).ready(function() {
 
-  $("#edu").fadeIn(2000);
-    setTimeout(function(){
-      $("#pandl").fadeIn(1700);
-    }, 1200);
+  edu.fadeIn(1500);
+  setTimeout(function(){
+    pandl.fadeIn(1000);
+  }, 1200);
+  
+  setTimeout(function(){
+    pandl.fadeOut(1000);
+    edu.fadeOut(2500);
+  },3500);
+
+  setTimeout(function(){
+    pedul_logo.fadeOut(700);
+  }, 5500);
     
-    setTimeout(function(){
-      edu.fadeOut(2500)
-      pandl.fadeOut(1000)
-    },3500);
-    
+  setTimeout(function(){
+    pedul_head.fadeIn(1000);
+  }, 6500);
+
+  setTimeout(function(){
+    intro_text.fadeIn(1000);
+  }, 6000);
+
 });
