3a915be0dcbf40e19099076dcb140315a0087073
Johnny Pacia <johnny.pacia@gmail.com>
2016-07-25 14:20:55 -0400

    adds tagline functionality

--- js/animate_logo.js
+++ js/animate_logo.js
@@ -1,12 +1,19 @@
 var edu = $("#edu");
 var pandl =  $("#pandl");
 var pedul_logo = $("#pedul_logo_container");
+var nav_bar = $(".navbar.navbar-default.navbar-fixed-top");
 var intro_text = $(".intro-text");
 var pedul_head = $("a.page-scroll.log");
+var background_image = $("header");
+var tag_line = $(".intro-heading");
+
 pedul_head.hide();
 intro_text.hide();
 edu.hide();
 pandl.hide();
+nav_bar.hide();
+background_image.hide();
+
 
 $(document).ready(function() {
 
@@ -16,20 +23,26 @@ $(document).ready(function() {
   }, 1200);
   
   setTimeout(function(){
-    pandl.fadeOut(1000);
-    edu.fadeOut(2500);
+    pandl.fadeOut(800);
+    edu.fadeOut(1500);
   },3500);
 
   setTimeout(function(){
-    pedul_logo.fadeOut(700);
+    pedul_logo.fadeOut(1000);
   }, 5500);
     
   setTimeout(function(){
-    pedul_head.fadeIn(1000);
-  }, 6500);
+    pedul_head.fadeIn(1900);
+    intro_text.fadeIn(1900);  
+  }, 5500);
 
   setTimeout(function(){
-    intro_text.fadeIn(1000);
+    background_image.fadeIn(7000);
+    pedul_head.fadeIn(1900);
+    nav_bar.fadeIn(1900);
+    tag_line.fadeIn(1000);
   }, 6000);
+
+  
 
 });
